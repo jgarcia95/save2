e282e65eabdc9185979f06ce8a528bf781852354
Joel Garcia <jgarcia95@hotmail.com>
2017-03-07 09:19:15 -0500

    xxx
    second test

---
+++ ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  save
+//
+//  Created by Joel Garcia on 3/6/17.
+//  Copyright © 2017 Joel Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  print("hello MOFU")
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    
+    print("just testing second part")
+    
+  }
+
+
+}
+
